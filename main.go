001542ba9f57b83c21fbd1a55831331fbfbde410
Elijah Prince <elijahprince73@gmail.com>
2022-03-27 21:35:00 -0700

    documenting

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+// Defines a package that can be compiled and executed
+// Must have a func called 'main'
 package main
 
+// Imports other packages
 import "fmt"
 
+// Logic of file that does something
 func main() {
 	fmt.Println("Hi there")
 }
